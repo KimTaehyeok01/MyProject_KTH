684aa7c2c7a0bc21ed2e09cfa4b08d9fa7103495
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-01-28 15:40:42 +0900

    타입 출력문

---
+++ java/javaExamples/src/main/java/org/example/ex02.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class ex02 {
+    public static void main(String[] args) {
+        System.out.println("화면출력");
+        System.out.println("화면" + "출력");
+        System.out.println("10" + 10);
+        System.out.println(10 + 10);
+
+        System.out.println("안녕하세요");
+
+        // 형식화된 출력문(formatted print)
+        System.out.printf("나이 : %d \n", 20);
+        System.out.printf("인사 : %s \n", "안녕");
+        System.out.printf("대답 : %c \n", '응');
+        System.out.printf("몸무게 : %.1f \n", 71.2);
+        System.out.printf("지수형 표현 e : %e \n", 200.0);
+        //자릿수 맞추기
+        System.out.printf("%5d\n", 123); // 5자릿수(공백으로 맞춤)
+        System.out.printf("%05d\n", 123); // 5자릿수(0으로 맞춤)
+        System.out.printf("%.2f\n", 123.45); // 5자릿수(0으로 맞춤)
+    }
+}
