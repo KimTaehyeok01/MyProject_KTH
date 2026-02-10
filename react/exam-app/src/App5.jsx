3073c5973518c0e7447b7b4245ddbb2600dc1249
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-10 16:10:21 +0900

    Update

--- react/exam-app/src/App5.jsx
+++ react/exam-app/src/App5.jsx
@@ -1,14 +1,20 @@
 // 리액트 8이후로 생략가능. React함수 직접 사용시에만 필요.
 import React from "react";
 
-import * as E from "./5-state/State";
+import * as E from "./5-state/Ex";
 
 function App() {
   return (
     <>
       {/* <E.Counter /> */}
       {/* <E.LikeButton /> */}
-      <E.TextMirror />
+      {/* <E.TextMirror /> */}
+      {/* <E.UserForm /> */}
+      <E.ColorButton />
+      <E.CheckBox />
+      <E.NumberCount />
+      <E.ListAdd />
+      <E.ArrayList />
     </>
   );
 }
