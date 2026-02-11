bc8ebb3c694576b8837aaedf268c046892d749db
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-11 15:49:09 +0900

    update

---
+++ react/exam-app/src/App6.jsx
@@ -0,0 +1,12 @@
+// 리액트 8이후로 생략가능. React함수 직접 사용시에만 필요.
+import React from "react";
+import * as E from "./6-effect/Effect";
+
+function App() {
+  return (
+    <>
+      <E.LifeCycleClass />
+    </>
+  );
+}
+export default App;
--- react/exam-app/src/main.jsx
+++ react/exam-app/src/main.jsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 // import App from "./App1.jsx";
 // import App from "./App2.jsx";
 // import App from "./App4.jsx";
-import App from "./App5.jsx";
+import App from "./App6.jsx";
 
 createRoot(document.getElementById("root")).render(<App />);
