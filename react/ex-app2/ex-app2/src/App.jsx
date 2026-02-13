d7d307f25421961f77c425ad29e3424f52617a04
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-13 15:55:27 +0900

    리액트로 만든 간단한 계산프로그램

---
+++ react/ex-app2/ex-app2/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// 계산기 프로그램 만들기
+
+function App() {
+  const [inputNumber1, setInputNumber1] = useState();
+  const [inputNumber2, setInputNumber2] = useState();
+  const [input, setInput] = useState("");
+
+  const handleAdd = () => {
+    setInput(Number(inputNumber1) + Number(inputNumber2));
+  };
+
+  const handlesubStr = () => {
+    setInput(Number(inputNumber1) - Number(inputNumber2));
+  };
+
+  const handleMa = () => {
+    setInput(Number(inputNumber1) * Number(inputNumber2));
+  };
+
+  const handleMu = () => {
+    setInput(Number(inputNumber1) / Number(inputNumber2));
+  };
+
+  const handleDelete = () => {
+    setInputNumber1("");
+    setInputNumber2("");
+    setInput("");
+  };
+
+  return (
+    <div className="card">
+      <h1 className="card-title">React App</h1>
+      <p className="clac-title">계산기 프로그램을 작성해보자</p>
+
+      <div className="input-number1">
+        <p className="num1">숫자 1</p>
+        <input
+          type="number"
+          id="input-num1"
+          value={inputNumber1}
+          onChange={(e) => setInputNumber1(e.target.value)}
+        />
+      </div>
+
+      <div className="input-number2">
+        <p className="num2">숫자 2</p>
+        <input
+          type="number"
+          id="input-num2"
+          value={inputNumber2}
+          onChange={(e) => setInputNumber2(e.target.value)}
+        />
+      </div>
+
+      <div className="input-result">
+        <p className="num-result">연산 결과 </p>
+        <input type="number" id="input-num-result" value={input} disabled />
+      </div>
+
+      <div className="btn-clac">
+        <button type="button" className="btn" onClick={handleAdd}>
+          덧셈
+        </button>
+        <button type="button" className="btn" onClick={handlesubStr}>
+          뺄셈
+        </button>
+        <button type="button" className="btn" onClick={handleMa}>
+          곱셈
+        </button>
+        <button type="button" className="btn" onClick={handleMu}>
+          나눗셈
+        </button>
+
+        <button type="button" className="delete" onClick={handleDelete}>
+          지우기
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
