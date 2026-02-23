ebdbb5ef8dfac1c4802286bc4532a32b661daa49
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-23 12:27:39 +0900

    Update MainController.java

--- springboot/Ex07Thymeleaf/src/main/java/com/study/Ex07Thymeleaf/MainController.java
+++ springboot/Ex07Thymeleaf/src/main/java/com/study/Ex07Thymeleaf/MainController.java
@@ -1,10 +1,17 @@
 package com.study.Ex07Thymeleaf;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -28,15 +35,49 @@ public class MainController {
     @RequestMapping(value = "/index2", method = RequestMethod.GET)
     public String index2(Model model){
         model.addAttribute("address_null", null);
+        model.addAttribute("address","서울");
+        model.addAttribute("address_empty","");
         return "index2";
     }
-
-    @GetMapping("index3")
+    @RequestMapping(value = "/index3", method = RequestMethod.GET)
     public String index3(Model model){
-        model.addAttribute("name_test","김태혁");
-        model.addAttribute("name_test","<ins>김태혁</ins>");
-        model.addAttribute("age_test", 29);
-        model.addAttribute("address_null", null);
+        model.addAttribute("standardDate", new Date()) ;
+        model.addAttribute("localDate", LocalDate.now());
+        model.addAttribute("localDateTime", LocalDateTime.now());
+
+        model.addAttribute("number1", 12345678);
+        model.addAttribute("number2", 123456.789);
         return "index3";
     }
+
+    @RequestMapping(value = "/index4", method = RequestMethod.GET)
+    public String index4(Model model){
+        // role : 역할 admin, member, guest -> 스프링 시큐리티(보안)
+        model.addAttribute("role", "admin");
+        return "index4";
+    }
+
+    @RequestMapping(value = "/index5", method = RequestMethod.GET)
+    public String index5(Model model){
+        Member member = new Member("Hong", "1234");
+        model.addAttribute("member", member);
+
+        List<Member> list = new ArrayList<>();
+        list.add(new Member("lee", "1111"));
+        list.add(new Member("hana", "2222"));
+        list.add(new Member("tom", "3333"));
+        model.addAttribute("list", list);
+        return "index5";
+    }
+
+    @RequestMapping(value = "/index6", method = RequestMethod.GET)
+    public String index6(Model model){
+        return "index6";
+    }
+
+    @RequestMapping(value = "/index7", method = RequestMethod.GET)
+    public String index7(Model model){
+        return "index7";
+    }
+
 }
