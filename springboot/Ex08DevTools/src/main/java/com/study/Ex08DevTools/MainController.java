601b915eeacc013538a666d3de0dfc8aab3fe822
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-23 14:55:26 +0900

    DevTool

---
+++ springboot/Ex08DevTools/src/main/java/com/study/Ex08DevTools/MainController.java
@@ -0,0 +1,14 @@
+package com.study.Ex08DevTools;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main(){
+        System.out.println("main()함수 호출됨");
+        return "index";
+    }
+}
