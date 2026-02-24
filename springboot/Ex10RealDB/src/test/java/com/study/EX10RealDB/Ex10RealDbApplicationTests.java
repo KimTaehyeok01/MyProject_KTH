47a24a1de1966b2c10b326e42724b5d42688af0f
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-24 09:34:57 +0900

    JPA

---
+++ springboot/Ex10RealDB/src/test/java/com/study/EX10RealDB/Ex10RealDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.EX10RealDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex10RealDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
