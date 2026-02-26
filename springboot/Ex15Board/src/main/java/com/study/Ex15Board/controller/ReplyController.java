340c25578522e49149aee46716fbc093dce1ef39
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-02-26 15:47:51 +0900

    CRUD를 사용한 게시판

--- springboot/Ex15Board/src/main/java/com/study/Ex15Board/controller/ReplyController.java
+++ springboot/Ex15Board/src/main/java/com/study/Ex15Board/controller/ReplyController.java
@@ -1,9 +1,36 @@
 package com.study.Ex15Board.controller;
 
+import com.study.Ex15Board.dto.ReplySaveRequestDto;
+import com.study.Ex15Board.service.ReplyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/reply")
+@RequiredArgsConstructor
 public class ReplyController {
+    private final ReplyService replyService;
+
+    @GetMapping("/deleteAction")
+    @ResponseBody
+    public String deleteAction(@RequestParam Long replyIdx, Long boardIdx) {
+        replyService.delete(replyIdx);
+        return "<script>alert('댓글 삭제 완료!'); location.href='/board/contentForm?boardIdx=" + boardIdx + "';</script>";
+    }
+
+    @PostMapping("/writeReplyAction")
+    @ResponseBody
+    public String writeReplyAction(@ModelAttribute ReplySaveRequestDto dto
+            , @RequestParam Long replyBoardIdx) {
+        Long newReplyIdx = replyService.save(dto);
+
+        boolean isFound = replyService.existsById(newReplyIdx);
+        if (isFound) {
+            return "<script>alert('댓글쓰기 성공!'); location.href='/board/contentForm?boardIdx=" + replyBoardIdx + " ';</script>";
+        } else {
+            return "<script>alert('댓글쓰기 실패'); history.back();</script>";
+        }
+    }
 }
--- springboot/Ex15Board/src/main/java/com/study/Ex15Board/service/ReplyService.java
+++ springboot/Ex15Board/src/main/java/com/study/Ex15Board/service/ReplyService.java
@@ -3,6 +3,7 @@ package com.study.Ex15Board.service;
 import com.study.Ex15Board.domain.reply.Reply;
 import com.study.Ex15Board.domain.reply.ReplyRepository;
 import com.study.Ex15Board.dto.ReplyResponseDto;
+import com.study.Ex15Board.dto.ReplySaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +25,24 @@ public class ReplyService {
         return list.stream().map(ReplyResponseDto::new).collect(Collectors.toList());
     }
 
-    // 댓글 삭제
-    // 댓글 저장
-    // 댓글 있는지 조회
+    //댓글 삭제
+    @Transactional
+    public void delete(Long replyIdx){
+        Reply entity = replyRepository.findById(replyIdx)
+                .orElseThrow( () -> new IllegalArgumentException("댓글 인덱스 오류입니다. replyIdx:"+replyIdx) );
+        replyRepository.delete( entity) ;
+    }
+    //댓글 저장
+    @Transactional
+    public Long save(final ReplySaveRequestDto dto){
+        Reply entity = replyRepository.save( dto.toEntity() );
+        return entity.getReplyIdx();
+    }
+    //댓글 있는지 조회
+    @Transactional(readOnly = true)
+    public boolean existsById(final Long replyIdx) {
+        boolean isFound = replyRepository.existsById( replyIdx );
+        return isFound;
+    }
 
 }
