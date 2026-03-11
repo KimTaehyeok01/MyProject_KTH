366613148b272e9fccce204add539f34316a1594
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-03-11 13:18:45 +0900

    Update SecurityConfig.java

--- springboot/Blog/src/main/java/com/Blog/Blog/login/SecurityConfig.java
+++ springboot/Blog/src/main/java/com/Blog/Blog/login/SecurityConfig.java
@@ -26,32 +26,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/user/signup", "/loginRedirect").permitAll()
-                .anyRequest().authenticated()
-            )
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/user/signup", "/loginRedirect").permitAll()
+                        .anyRequest().authenticated()
+                )
 
-            // 폼 로그인 설정
-            .formLogin(login -> login
-                .loginPage("/")                        
-                .loginProcessingUrl("/login")          
-                .usernameParameter("userId")         
-                .passwordParameter("userPassword")     
-                .successHandler(successHandler)        // 로그인 성공 
-                .failureUrl("/?error=true")   // 로그인 실패 시 이동
-                .permitAll()
-            )
+                // 폼 로그인 설정
+                .formLogin(login -> login
+                        .loginPage("/")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("userId")
+                        .passwordParameter("userPassword")
+                        .successHandler(successHandler)        // 로그인 성공
+                        .failureUrl("/?error=true")   // 로그인 실패 시 이동
+                        .permitAll()
+                )
 
-            // 로그아웃 설정 
-            .logout(logout -> logout
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) 
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll()
-            )
-            .userDetailsService(userDetailsService);
+                // 로그아웃 설정
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                )
+                .userDetailsService(userDetailsService);
 
         return http.build();
     }
