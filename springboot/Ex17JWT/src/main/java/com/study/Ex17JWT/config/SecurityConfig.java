67e5a0f64885c6d6dee5cf5996cfa99a20a80f23
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-03-23 11:20:07 +0900

    JWT

--- springboot/Ex17JWT/src/main/java/com/study/Ex17JWT/config/SecurityConfig.java
+++ springboot/Ex17JWT/src/main/java/com/study/Ex17JWT/config/SecurityConfig.java
@@ -21,51 +21,52 @@ import java.util.Collections;
 @EnableMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
-  private final JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
 
-  @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        //RestAPI 서버에서는 주로 비활성화한다.
-        //인증을 폼 기반으로 하지 않고, 토큰(JWT)기반으로 하기에, CSRF 인증이 불필요하다.
-        .csrf(csrf -> csrf.disable());
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // cors 설정을 해주는 코드
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                //RestAPI 서버에서는 주로 비활성화한다.
+                //인증을 폼 기반으로 하지 않고, 토큰(JWT)기반으로 하기에, CSRF 인증이 불필요하다.
+                .csrf(csrf -> csrf.disable());
 
-    http
-        .authorizeHttpRequests((authz) -> authz
-            // "/**" 하위 경로 포함, 모든 루트 경로의 인가를 풀어준다.
-            .requestMatchers("/**")
-            .permitAll());
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        // "/**" 하위 경로 포함, 모든 루트 경로의 인가를 풀어준다.
+                        .requestMatchers("/**")
+                        .permitAll());
 
-    //스프링 시큐리티에서 세션관리(html기반) 정책을 관리안함 으로 설정한다.
-    //JWT 토큰 기반 서버는 세션관리는 토큰으로 하기 때문에
-    http.sessionManagement(sessionConfig -> sessionConfig
-        .sessionCreationPolicy(
-            SessionCreationPolicy.STATELESS
-        )
-    );
-    //JWT 보안 필터를 특정 필터 앞에 추가한다.
-    http.addFilterBefore(new JwtAuthenticationFilter(jwtUtil),
-        UsernamePasswordAuthenticationFilter.class
-    );
+        //스프링 시큐리티에서 세션관리(html기반) 정책을 관리안함 으로 설정한다.
+        //JWT 토큰 기반 서버는 세션관리는 토큰으로 하기 때문에
+        http.sessionManagement(sessionConfig -> sessionConfig
+                .sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )
+        );
+        //JWT 보안 필터를 특정 필터 앞에 추가한다.
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtUtil),
+                UsernamePasswordAuthenticationFilter.class
+        );
 
-    return http.build();
-  }
-  //CORS 설정
+        return http.build();
+    }
+    //CORS 설정
 
-  //CORS 설정
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedHeaders(Collections.singletonList("*")); // 허용할 HTTP header
-    config.setAllowedMethods(Collections.singletonList("*")); // 허용할 HTTP method
+    //CORS 설정
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedHeaders(Collections.singletonList("*")); // 허용할 HTTP header
+        config.setAllowedMethods(Collections.singletonList("*")); // 허용할 HTTP method
 //    config.setAllowedOriginPatterns(Collections.singletonList("*")); // 허용할 출처
-    config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:5173")); // 허용할 출처
-    config.setAllowCredentials(true); // 쿠키 인증 요청 허용
+        config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:5173")); // 허용할 출처
+        config.setAllowCredentials(true); // 쿠키 인증 요청 허용
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-    return source;
-  }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
 }//class
--- springboot/Ex17JWT/src/main/resources/db.sql
+++ springboot/Ex17JWT/src/main/resources/db.sql
@@ -1,11 +1,15 @@
+
 USE mydb;
 
 drop table users_jwt;
 create table users_jwt(
-                          id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                          email 		VARCHAR(255) NOT NULL,
-                          password    VARCHAR(255) NOT NULL,
-                          user_role   VARCHAR(255) DEFAULT 'ROLE_USER'
+    id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    user_role   VARCHAR(255) DEFAULT 'ROLE_USER'
 );
 
 SELECT * from users_jwt;
+DESC users_jwt;
+
+DELETE FROM users_jwt;
