9d988e597cd9659535ee1a3c3b5f74635d2210de
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-03-23 09:54:17 +0900

    JWT

--- springboot/Ex17JWT/src/main/java/com/study/Ex17JWT/entity/UsersRepo.java
+++ springboot/Ex17JWT/src/main/java/com/study/Ex17JWT/entity/UsersRepo.java
@@ -7,13 +7,13 @@ import java.util.Optional;
 
 @Repository
 public interface UsersRepo extends JpaRepository<Users, Long> {
-  //1. 기본함수 : findAll, findById, save, count, deleteById
-  //2. 사용자정의함수 : 열이름, DB부사절(where, or, and, orderby, desc, asc, limit, groupby)
-  //                   김수한무... 이름이 너무 길어지는 단점.
-  // SQL Query : select * from users_jwt where email=:email
-  Optional<Users> findByEmail(String email);
-  // SQL QUery : select * from users_jwt where email=:email and password=:password
-  Optional<Users> findByEmailAndPassword(String email, String password);
-  //3. @Query : native Query(AnsiSQL), JPQL(엔티티)
+    //1. 기본함수 : findAll, findById, save, count, deleteById
+    //2. 사용자정의함수 : 열이름, DB부사절(where, or, and, orderby, desc, asc, limit, groupby)
+    //                   김수한무... 이름이 너무 길어지는 단점.
+    // SQL Query : select * from users_jwt where email=:email
+    Optional<Users> findByEmail(String email);
+    // SQL QUery : select * from users_jwt where email=:email and password=:password
+    Optional<Users> findByEmailAndPassword(String email, String password);
+    //3. @Query : native Query(AnsiSQL), JPQL(엔티티)
 
 }
